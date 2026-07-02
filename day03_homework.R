0f88d73d8fd02d43fd05cd9a71889a25040241df
knghiroki <kunigohiroki@gmail.com>
2026-07-02 12:49:07 +0900

    commit

--- day03_homework.R
+++ day03_homework.R
@@ -1,6 +1,6 @@
 # DAY 3 HOMEWORK
 #
-# Name: Joel Nitta
+# Name: Hiroki Kunigo
 
 # 0. This demonstrates how to save your answers.
 # I've written the answer for you.
@@ -27,26 +27,33 @@ answer_0 <- 365 * 5
 
 weight_lb <- 160
 height_ft <- 6
+weight_kg <- weight_lb*0.4536
+height_m <- height_ft*0.3048
+weight_kg
+height_m
+answer_1<-weight_kg / (height_m * height_m)
+answer_1
 
 # 2. The following code has an error. Fix it by editing the code.
 # (You don't need to save a separate answer for this one, just fix the code.)
-
 volume_oz <- 130
-volume_ml <- Volume_oz * 29.574
-
+answer_2 <- volume_oz * 29.574
+answer_2
 # 3. What is the value of z after running the following code?
-#
 # Save your answer in an object called answer_3
 
 x <- 2
 y <- 3
 z <- x + y
 x <- 5
+answer_3 <- z
+answer_3
 
 # 4. The sqrt() function calculates the square root of a number.
 #
 # Use the sqrt() function to calculate the square root of 200, and
 # save your answer in an object called answer_4.
+answer_4 <- sqrt(200)
 
 # 5. The seq() function generates sequences of numbers.
 # Its arguments include "from" (the starting number), "to"
@@ -55,6 +62,7 @@ x <- 5
 #
 # Use seq() make a sequence of even numbers starting at 2 and ending at 100, and
 # save your answer in an object called answer_5
+answer_5 <- seq(from =2 , to =100 , by =2 )
 
 # 6. Make a vector of logical values (TRUE and FALSE), and save it to an
 #  object called lgl_vec
@@ -68,7 +76,11 @@ x <- 5
 #
 # HINT: use c() to make vectors
 # HINT: characters are always written with quotation marks
-
+lgl_vec <- c(TRUE, FALSE)
+chr_vec <- c("beef","chicken")
+mixed_vec <- c(lgl_vec, chr_vec)
+answer_6 <- typeof(mixed_vec)
+answer_6
 # 7. R includes some pre-defined vectors, like `letters`:
 letters
 
@@ -78,3 +90,6 @@ letters
 # Save your answer in an object called answer_7
 #
 # HINT: Use `seq()` inside the square brackets
+letters
+answer_7 <- letters(seq(from=1, to=26, by=2))
+answer_7
